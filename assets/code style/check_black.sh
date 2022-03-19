c3b5ac5c372070fd3076c1804baf98ed8e4d27e1
Manuel Bieri <manuel.bieri@outlook.com>
2022-03-19 18:48:38 +0100

    Adjust code style check.

--- assets/code style/check_black.sh
+++ assets/code style/check_black.sh
@@ -1,1 +1,1 @@
-python -m black ./../../Fumagalli_Motta_Tarantino_2020 --line-length 120 --diff
+python -m black ./../../Fumagalli_Motta_Tarantino_2020 --line-length 120 --diff  --extend-exclude ./../../Notebooks
