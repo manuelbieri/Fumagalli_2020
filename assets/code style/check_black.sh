d3e9d18463ef2b5c229ddef4f14495b598f56aa8
Manuel Bieri <manuel.bieri@outlook.com>
2022-03-19 20:47:14 +0100

    Add setup.py to code style check.

--- assets/code style/check_black.sh
+++ assets/code style/check_black.sh
@@ -3,3 +3,6 @@ python -m black ./../../Fumagalli_Motta_Tarantino_2020 --line-length 120 --diff 
 
 printf '\n Check Tests: ... '
 python -m black ./../../Test_Fumagalli_Motta_Tarantino_2020 --line-length 120 --diff  --extend-exclude ./../../Notebooks
+
+printf '\n Check Setup.py: ... '
+python -m black ./../../setup.py --line-length 120 --diff --extend-exclude ./../../Notebooks
--- assets/code style/run_black.sh
+++ assets/code style/run_black.sh
@@ -3,3 +3,6 @@ python -m black ./../../Fumagalli_Motta_Tarantino_2020 --line-length 120 --exten
 
 printf '\n Format Tests: ... '
 python -m black ./../../Test_Fumagalli_Motta_Tarantino_2020 --line-length 120 --extend-exclude ./../../Notebooks
+
+printf '\n Format Setup.py: ... '
+python -m black ./../../setup.py --line-length 120 --extend-exclude ./../../Notebooks
