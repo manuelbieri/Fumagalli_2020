3379c2e7558270360a84792e5b50a68b98e2501a
Manuel Bieri <manuel.bieri@outlook.com>
2022-03-19 19:54:54 +0100

    Adjust code style checks.

--- assets/code style/check_black.sh
+++ assets/code style/check_black.sh
@@ -1,2 +1,5 @@
+printf 'Check Package: ... '
 python -m black ./../../Fumagalli_Motta_Tarantino_2020 --line-length 120 --diff  --extend-exclude ./../../Notebooks
+
+printf '\n Check Tests: ... '
 python -m black ./../../Test_Fumagalli_Motta_Tarantino_2020 --line-length 120 --diff  --extend-exclude ./../../Notebooks
--- assets/code style/run_black.sh
+++ assets/code style/run_black.sh
@@ -1,2 +1,5 @@
+printf 'Format Package: ... '
 python -m black ./../../Fumagalli_Motta_Tarantino_2020 --line-length 120 --extend-exclude ./../../Notebooks
+
+printf '\n Format Tests: ... '
 python -m black ./../../Test_Fumagalli_Motta_Tarantino_2020 --line-length 120 --extend-exclude ./../../Notebooks
