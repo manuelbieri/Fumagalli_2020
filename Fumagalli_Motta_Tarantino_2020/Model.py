430402c2b5abd149c0853dda0942b45f75e12254
Manuel Bieri <manuel.bieri@outlook.com>
2022-04-11 16:02:03 +0200

    Extract helper methods for assumption checks.

--- Fumagalli_Motta_Tarantino_2020/Model.py
+++ Fumagalli_Motta_Tarantino_2020/Model.py
@@ -130,11 +130,7 @@ class BaseModel:
         assert (
             self.w_without_innovation < self.w_with_innovation < self.w_duopoly
         ), "Ranking of total welfare not valid (p.7)"
-        assert (
-            self._success_probability
-            * (self.w_with_innovation - self.w_without_innovation)
-            > self._development_costs
-        ), "A4 not satisfied (p.8)"
+        self._check_assumption_four()
 
     def _check_preconditions(self):
         # preconditions given (p.6-8)
@@ -156,28 +152,47 @@ class BaseModel:
         assert (
             self.cs_with_innovation >= self.cs_without_innovation
         ), "Consumer surplus with the innovation has to weakly bigger than without the innovation"
+        self._check_assumption_one()
+        self._check_assumption_two()
+        self._check_assumption_three()
+        self._check_assumption_five()
+
+    def _check_assumption_five(self):
         assert (
-            self.incumbent_profit_with_innovation
-            > self.incumbent_profit_duopoly + self.startup_profit_duopoly
-        ), "A1 not satisfied (p.7)"
-        assert (
-            self.startup_profit_duopoly
-            > self.incumbent_profit_with_innovation
-            - self.incumbent_profit_without_innovation
-        ), "A2 not satisfied (p.7)"
-        assert (
-            self.success_probability * self.startup_profit_duopoly
-            > self._development_costs
-        ), "A3 not satisfied (p.8)"
-        assert (
-            self.private_benefit - self.development_costs
-            < 0
-            < self.private_benefit
-            - (
-                self.success_probability * self._startup_profit_duopoly
-                - self.development_costs
-            )
+                self.private_benefit - self.development_costs
+                < 0
+                < self.private_benefit
+                - (
+                        self.success_probability * self._startup_profit_duopoly
+                        - self.development_costs
+                )
         ), "A5 not satisfied (p.8)"
+
+    def _check_assumption_four(self):
+        assert (
+                self._success_probability
+                * (self.w_with_innovation - self.w_without_innovation)
+                > self._development_costs
+        ), "A4 not satisfied (p.8)"
+
+    def _check_assumption_three(self):
+        assert (
+                self.success_probability * self.startup_profit_duopoly
+                > self._development_costs
+        ), "A3 not satisfied (p.8)"
+
+    def _check_assumption_two(self):
+        assert (
+                self.startup_profit_duopoly
+                > self.incumbent_profit_with_innovation
+                - self.incumbent_profit_without_innovation
+        ), "A2 not satisfied (p.7)"
+
+    def _check_assumption_one(self):
+        assert (
+                self.incumbent_profit_with_innovation
+                > self.incumbent_profit_duopoly + self.startup_profit_duopoly
+        ), "A1 not satisfied (p.7)"
 
     @property
     def tolerated_harm(self) -> float:
