4a286031b3931c901e83205284d651456f795503
Manuel Bieri <manuel.bieri@outlook.com>
2022-05-29 17:25:10 +0200

    Update setup.py for release.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 setup(
     name="Fumagalli_Motta_Tarantino_2020",
     packages=find_packages(exclude=["Notebooks"]),
-    version="0.2.1",  # change with new version
+    version="0.3.1",  # change with new version
     license="MIT",
     description="Implements the models presented in Fumagalli et al. (2020)",
     long_description=long_description,
@@ -23,7 +23,7 @@ setup(
         "Download": "https://github.com/manuelbieri/Fumagalli_2020/releases",
         "Source": "https://github.com/manuelbieri/Fumagalli_2020",
     },
-    download_url="https://github.com/manuelbieri/Fumagalli_2020/archive/refs/tags/v0.2.1.tar.gz",  # change with new version
+    download_url="https://github.com/manuelbieri/Fumagalli_2020/archive/refs/tags/v0.3.1.tar.gz",  # change with new version
     keywords=["Killer Acquisition", "Competition", "Innovation"],
     classifiers=[
         "Development Status :: 4 - Beta",  # "3 - Alpha" / "4 - Beta" / "5 - Production/Stable"
