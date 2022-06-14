cc8d6c3d977302e6d362d0982e592ec018a37352
Manuel Bieri <mail@manuelbieri.ch>
2022-06-14 15:55:03 +0200

    Adjusted import structure.

--- Fumagalli_Motta_Tarantino_2020/Configurations/__init__.py
+++ Fumagalli_Motta_Tarantino_2020/Configurations/__init__.py
@@ -2,3 +2,4 @@
 .. include:: README.md
 """
 from Fumagalli_Motta_Tarantino_2020.Configurations.LoadConfig import *
+from Fumagalli_Motta_Tarantino_2020.Configurations.ConfigExceptions import *
--- Fumagalli_Motta_Tarantino_2020/__init__.py
+++ Fumagalli_Motta_Tarantino_2020/__init__.py
@@ -11,4 +11,4 @@ from Fumagalli_Motta_Tarantino_2020.VisualizeRanges import *
 from Fumagalli_Motta_Tarantino_2020.Project import *
 
 import Fumagalli_Motta_Tarantino_2020.Exceptions as Exceptions
-import Fumagalli_Motta_Tarantino_2020.Distributions as Utilities
+import Fumagalli_Motta_Tarantino_2020.Distributions as Distributions
