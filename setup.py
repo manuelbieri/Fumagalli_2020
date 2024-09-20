26074d4d1db8cac43866cc36cd6778110d42cab6
Manuel Bieri <mail@manuelbieri.ch>
2024-09-20 15:39:36 +0200

    Update setup and dependencies.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 setup(
     name="Fumagalli_Motta_Tarantino_2020",
     packages=find_packages(),
-    version="1.0.1",  # adjust
+    version="1.0.2",  # adjust
     license="MIT",
     description="Implements the models presented in Fumagalli et al. (2020)",
     long_description=long_description,
@@ -23,29 +23,29 @@ setup(
         "Download": "https://github.com/manuelbieri/Fumagalli_2020/releases",
         "Source": "https://github.com/manuelbieri/Fumagalli_2020",
     },
-    download_url="https://github.com/manuelbieri/Fumagalli_2020/archive/refs/tags/v1.0.1.tar.gz",  # adjust
+    download_url="https://github.com/manuelbieri/Fumagalli_2020/archive/refs/tags/v1.0.2.tar.gz",  # adjust
     keywords=["Killer Acquisition", "Competition", "Innovation"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",  # "3 - Alpha" / "4 - Beta" / "5 - Production/Stable"
         "Intended Audience :: Developers",
         "Topic :: Software Development",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12",
     ],
     install_requires=[
-        "scipy>=1.8.1",
-        "matplotlib>=3.5.2",
-        "numpy>=1.23.1",
-        "ipython>=7.34.0",
-        "jupyter~=1.0.0",
-        "mockito~=1.3.3",
+        "scipy>=1.14.1",
+        "matplotlib>=3.9.2",
+        "numpy>=2.1.1",
+        "ipython>=8.27.0",
+        "jupyter~=1.1.1",
+        "mockito~=1.5.1",
+        "ipympl==0.9.4",
     ],  # adjust
     extras_require={
-        "docs": "pdoc~=12.0.2",
+        "docs": "pdoc~=14.7.0",
         "style": "SciencePlots>=1.0.9",
-        "black": ["black>=22.6.0", "jupyter-black>=0.3.1"],
-        "interactive": "ipywidgets>=7.7.1",
+        "black": ["black>=24.8.0", "jupyter-black>=0.4.0"],
+        "interactive": "ipywidgets>=7.8.4",
     },
     package_data={
         "Fumagalli_Motta_Tarantino_2020.Configurations": ["params.csv"],
